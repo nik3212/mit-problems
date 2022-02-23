1d477b8eaffa40ed59fcdfb05fdc0c439d0d312a
Nikita Vasilev <nv3212@gmail.com>
2022-02-23 14:30:57 +0300

    Problem set 1

---
+++ problem_set_1/reorder_students.py
@@ -0,0 +1,13 @@
+def reorder_students(L):
+    '''
+    Input:  L    | linked list with head L.head and size L.size
+    Output: None |
+    This function should modify list L to reverse its last half.
+    Your solution should NOT instantiate:
+        - any additional linked list nodes
+        - any other non-constant-sized data structures
+    '''
+    ##################
+    # YOUR CODE HERE #
+    ##################
+    return
